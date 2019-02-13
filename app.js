630e347c8133a64fff5a24dc9cb3dd01694ec5c1
Samuel Marcus <smarcus4@gmu.edu>
2019-02-13 18:40:17 -0500

    working local storage

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 let startButton = document.getElementById("startApp");
 let container = document.querySelector(".todo");
-let items = document.querySelector(".itemHolder")
+let items = document.querySelector(".itemHolder");
+let output = document.querySelector("#output");
+output.style.visibility = "hidden";
+
+items.addEventListener("click", function (e) {
+    if (e.target.classList.contains("listClass")) {
+        if (!(e.target.classList.contains("removed"))) {
+            e.target.classList.add("removed");
+        } else {
+            e.target.classList.remove("removed");
+        }
+        console.log(e.target);
+    } else if (e.target.classList.contains("deleteClass")) {
+        e.target.parentNode.remove();
+        
+    }
+    storeItems();
+})
 
 
 startButton.addEventListener("click", function (i) {
@@ -16,17 +33,22 @@ startButton.addEventListener("click", function (i) {
     container.appendChild(userinput);
     container.appendChild(createItem);
     addItem(createItem);
+    displayValues();
     
 })
 
 function addItem(button) {
     button.addEventListener("click", function (e) {
         let getItems = document.getElementsByClassName("inputClass")[0].value;
-        if (getItems===" "){
-            alert("You must enter an item!!!");
+        console.log(getItems);
+        if (getItems.length<=0 || getItems==null || getItems=="          "){
+            output.style.visibility = "visible";
+            e.preventDefault;
         } else {
             createList(getItems);
-            document.getElementsByClassName("inputClass")[0].value= " "
+            document.getElementsByClassName("inputClass")[0].value = ""
+            output.style.visibility = "hidden";
+
         }
 
     })
@@ -42,29 +64,54 @@ function createList(addedItem) {
     items.appendChild(listItem);
     listItem.innerHTML = addedItem
     listItem.appendChild(deleteBtn);
-    markingList(listItem);
-    deleteItem(deleteBtn, listItem);
+    //markingList(listItem);
+    //deleteItem(deleteBtn, listItem);
+    storeItems();
+}
+
+
+//function markingList(newestItem) {
+//    newestItem.addEventListener("click", function (e) {
+//        if (newestItem.classList.contains("removed")) {
+//            this.classList.remove("removed");
+//        } else {
+//            this.classList.add("removed");
+//        }
+//        storeItems()
+
+//    })
+//}
+
+
+
+//function deleteItem(deletethis, theItem) {
     
+//    deletethis.addEventListener("click", function (e) {
+//        e.currentTarget.parentNode.remove();
+
+//        storeItems();
+//    },false)
+//}
+
+
+
+function storeItems() {
+
+    window.localStorage.myitems = items.innerHTML;
+
+}
+
+function displayValues() {
+    let storedVals = window.localStorage.myitems;
+
+    if (!storedVals) {
+        return false;
+    } else {
+        items.innerHTML = storedVals;
+    }
+
 }
 
 
-function markingList(newestItem) {
-    newestItem.addEventListener("click", function (e) {
-        if (newestItem.classList.contains("removed")) {
-            this.classList.remove("removed");
-        } else {
-            this.classList.add("removed");
-        }
-
-    })
-}
-
-
-
-function deleteItem(deletethis, theItem) {
-    
-    deletethis.addEventListener("click", function (e) {
-        e.currentTarget.parentNode.remove();
-    })
-}
+displayValues();
 
